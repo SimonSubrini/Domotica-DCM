a1d10b884ac5304c87b37e00ecac11e084af008b
SimonSubrini <simonsubrini@gmail.com>
2022-06-05 20:58:10 -0300

    Cree la clase "Slave" a partir de la cual voy a instanciar a todos los esclavos conectados al maestro

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ import Functions
 import ufirebase as firebase
 
 # Inicialización de variables
+""" 
+Cuando utilice el modulo "machine" el número correspondiente a cada pin
+lo tengo que sacar de la imagen "NodeMcu-PinOut". Estos números aparecen
+como "GPIO"+Número
+"""
 led = Pin(2, Pin.OUT)
+Button = Pin(16, Pin.IN)
+Cant_Slaves = 0
 
 
 def Config():
-    for i in range(5):
-        led.value(False)
-        time.sleep_ms(150)
-        led.value(not led.value())
-    Functions.Prints()
     ConnectWifi()
     ConnectFirebase()
 
@@ -23,7 +25,7 @@ def ConnectWifi():
     with open('./Wifi_Pass.txt', 'rt', encoding='utf-8') as file_object:
         Wifi_Pass = file_object.read()
     Redes = Wifi_Pass.split('\r\n\r\n')
-    for i in range(5):
+    for i in range(len(Redes)):
         WIFI_SSID = Redes[i].split('"')[1]
         WIFI_PW = Redes[i].split('"')[3]
         if LibWifi.connect_wifi(WIFI_SSID, WIFI_PW) == 1:
@@ -33,21 +35,36 @@ def ConnectWifi():
 def ConnectFirebase():
     with open('./Firebase_Pass.txt', 'rt', encoding='utf-8') as file_object:
         FireBase_Pass = file_object.read()
-
     Firebase_ID = FireBase_Pass.split('"')[1]
     Firebase_Auth = FireBase_Pass.split('"')[3]
-
     firebase.setURL("https://" + Firebase_ID + ".firebaseio.com/")
 
-    # firebase.put('Bool', False)
-    # firebase.put('Str', 'Simon')
-    # firebase.put('Int', 18)
-    # firebase.put('Float', 10.25)
-    # firebase.put('Strs', 'Hola')
+
+class Slave:
+    def __init__(self):
+        self.__Address = 0
+        self.__Consumption = 0  # En KW/H
+        self.__Type = ""
+
+    def ChangeAddress(self, NewAddress):
+        self.__Address = NewAddress
+        return "New Address: {}".format(self.__Address)
+
+    def ChangeType(self, NewType):
+        self.__Type = NewType
+        return "New Type: {}".format(self.__Type)
+
+    def ChangeConsumption(self, NewConsumption):
+        self.__Type = NewConsumption
+        return "New Consumption: {}".format(self.__Consumption)
 
 
 Config()
+with open('./Structure.json') as file_object:
+    Structure = json.load(file_object)
+firebase.put("Dispositivos", Structure)
+
 while 1:
-    with open('./Structure.json') as file_object:
-        Structure = json.load(file_object)
-    firebase.put("Dispositivos", Structure)
+    print('Estado boton: {}'.format(Button.value()))
+    led.value(not led.value())
+    time.sleep_ms(500)
